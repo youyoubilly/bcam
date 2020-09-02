246d1d41e34be72c28785fda4ae481f1655b91fe
Billio <youyoubilly@qq.com>
2020-09-02 17:37:58 +0800

    merge frame_dp into bcam

--- bcam.py
+++ bcam.py
@@ -10,6 +10,14 @@ import numpy as np
 import enum
 import cv2
 
+def bgr8_to_jpeg(value, quality=10):
+    return bytes(cv2.imencode('.jpg', value)[1])
+
+def frame_dp(camera_image):
+    image = np.copy(camera_image)
+    jpeg_image = bgr8_to_jpeg(image)
+    return jpeg_image
+
 class Camera(SingletonConfigurable):
     
     value = traitlets.Any()
